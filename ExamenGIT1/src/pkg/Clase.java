985e854d3c2fc3c91499db03ee9a33d88fcd205e
ricardoBendezuCeacfp <131450768+ricardoBendezuCeacfp@users.noreply.github.com>
2023-05-26 12:45:44 +0200

    Cambios generales en todas las ramas

--- ExamenGIT1/src/pkg/Clase.java
+++ ExamenGIT1/src/pkg/Clase.java
@@ -5,7 +5,8 @@ public class Clase {
 	String hola= "Hola soy Ricardo Andrés Bendezú";
 	int edad= 19;
 	double estatura = 1.95;
-	String hobbies= "Comer, dormir, jugar ,etc.";
+	String hobbies= "Estudiar,dormir, jugar ,etc.";
 	String cambio="Esto es un cambio para el git";
+	int numeroCambios= 80000;
 	
 }
--- ExamenGIT1/src/pkg2/Clase2.java
+++ ExamenGIT1/src/pkg2/Clase2.java
@@ -4,5 +4,5 @@ public class Clase2 {
 
 	String clase="Que rara esta clase nueva";
 	String paquete="Estoy en un paquete nuevo";	
-	
+	String cambio="Cambio en rama 2";
 }
