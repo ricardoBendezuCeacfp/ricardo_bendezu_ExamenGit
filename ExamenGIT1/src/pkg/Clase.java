5754e0b3c63538fb5a786d746e5813c0cb354bd2
ricardoBendezuCeacfp <131450768+ricardoBendezuCeacfp@users.noreply.github.com>
2023-05-26 12:36:07 +0200

    Cambios en la segunda rama a la clase.java del primer paquete y añadicion de nuevo paquete con nuevo archivo

--- ExamenGIT1/src/pkg/Clase.java
+++ ExamenGIT1/src/pkg/Clase.java
@@ -2,9 +2,9 @@ package pkg;
 
 public class Clase {
 	
-	String hola= "Hola soy Ricardo";
+	String hola= "Hola soy Ricardo Andrés Bendezú";
 	int edad= 19;
-	double estatura = 1.85;
+	double estatura = 1.95;
 	String hobbies= "Comer, dormir, jugar ,etc.";
 	String cambio="Esto es un cambio para el git";
 	
---
+++ ExamenGIT1/src/pkg2/Clase2.java
@@ -0,0 +1,8 @@
+package pkg2;
+
+public class Clase2 {
+
+	String clase="Que rara esta clase nueva";
+	String paquete="Estoy en un paquete nuevo";	
+	
+}
