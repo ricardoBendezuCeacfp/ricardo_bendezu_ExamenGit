2c358b763c714a9cfc0fdb62fbc16e94fb403afc
ricardoBendezuCeacfp <131450768+ricardoBendezuCeacfp@users.noreply.github.com>
2023-05-26 12:30:52 +0200

    Añadición de archivos

---
+++ ExamenGIT1/src/pkg/Clase.java
@@ -0,0 +1,11 @@
+package pkg;
+
+public class Clase {
+	
+	String hola= "Hola soy Ricardo";
+	int edad= 19;
+	double estatura = 1.85;
+	String hobbies= "Comer, dormir, jugar ,etc.";
+	String cambio="Esto es un cambio para el git";
+	
+}
